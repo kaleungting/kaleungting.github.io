d12ea5c087c0459384a0dc71e33d5e0ce2985224
Ken Ting <kaleungting@gmail.com>
2020-12-04 15:08:39 -0500

    Skills section more responsive

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+    $name=$_REQUEST['name'];
+    $email=$_REQUEST['email'];
+    $message=$_REQUEST['message'];
+    if (($name=="")||($email=="")||($message==""))
+        {
+        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+        }
+    else{        
+        $from="From: $name<$email>\r\nReturn-path: $email";
+        $subject="Message sent using your contact form";
+        mail("kaleungting@gmail.com", $subject, $message, $from);
+        echo "Email sent!";
+        }
+    }  
+?>
